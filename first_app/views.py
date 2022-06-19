a34c65c634f4899d78a1faf519976d1487fc951c
ankit0305 <jaiswal.ankit2000@gmail.com>
2022-06-19 17:49:02 +0530

    updating address

--- first_app/views.py
+++ first_app/views.py
@@ -6,7 +6,7 @@ import requests
 from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_CRAIGSLIST_URL = 'https://bangalore.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
